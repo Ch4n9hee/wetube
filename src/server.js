7a2e4cc796b6e1aaaa3445d4ce3c3b990793cae2
Ch4n9hee <hch9510@kbiohc.io>
2022-06-11 17:11:18 +0900

    3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-const handleListening = () => console.log(`Server Listeining on port http://localhost:${PORT} 🚀`);
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
-    return res.send("사랑해 여보❤️");
+    return res.send("Home");
 };
+app.use(logger);
+app.get("/", handleHome);
 
-app.get("/", logger, handleHome);
+
+const handleListening = () => console.log(`Server Listeining on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
 
