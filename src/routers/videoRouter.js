8fd19068a645277563c51cf27db62f951088cb89
Ch4n9hee <hch9510@kbiohc.io>
2022-06-15 11:59:25 +0900

    4.6 Planning Routes

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
+ 
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
 
-const handleWatchVideo = (req,res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 export default videoRouter
