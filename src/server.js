cef037625b1c13b252708049b49bd1c2744f0d2d
CHANGHEE HAN <79398161+Ch4n9hee@users.noreply.github.com>
2022-07-17 16:23:47 +0900

    Download Transcoded Video

--- src/server.js
+++ src/server.js
@@ -7,12 +7,18 @@ import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import { localsMiddleware } from "./middlewares";
+import apiRouter from "./routers/apiRouter";
 
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -29,5 +35,6 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;
